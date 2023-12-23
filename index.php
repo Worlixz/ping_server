24fe501cebf234014aab9946ce2e484253245f9b
Worlixz <geremydumas@gmail.com>
2023-12-24 00:02:13 +0100

    ajout de l'affichae graphique dans une pahe html

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
 <?php
 $sites = [
-    "https://www.saphael.fr",
-    "https://www.lesjardinsdestdenis.fr",
-    "https://www.google.com"
+    "Saphael" => "https://www.saphael.fr",
+    "Les jardins de st-denis" => "https://www.lesjardinsdestdenis.fr",
+    "Neosoma" => "https://www.neosoma.fr"
 ];
-
-foreach($sites as $site){
-    $handle = @fopen($site, "r");
-    if($handle){
-        echo("$site ok \n");
-    }else{
-        echo("$site down \n");
-    }
-}
-
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Status server</title>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h1>Status des serveurs : </h1>
+        </div>
+        <div class="container-cards">
+            <?php foreach($sites as $name => $site): ?>
+                <?php $handle = @fopen($site, 'r'); ?>
+                <div onclick='window.location.href="<?= $site ?>";' class="card">
+                    <h1><?= $name ?></h1>
+                    <div class="information-card">
+                        <p><?= $site ?></p>
+                        <div class="div-card-statut">
+                            <?php if($handle): ?>
+                                <p>Status :</p>
+                                <span id="span-statut" style="background: green;"></span>
+                            <?php else: ?>
+                                <p>Status :</p>
+                                <span id="span-statut" style="background: red;"></span>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
+    </div>
+</body>
+</html>
